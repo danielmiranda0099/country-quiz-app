dc470ec60a22a57766ff8834ba5f546d8ff27a96
dmc0099 <danielmesa877@gmail.com>
2021-09-10 23:29:32 -0500

    base project

---
+++ src/components/card-quiz/ButtonsAnswers.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const ButtonsAnswers = () => {
+
+    return(
+        <h2>ButtonsAnswers</h2>
+    )
+}
---
+++ src/components/card-quiz/CardQuiz.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { ButtonsAnswers } from './ButtonsAnswers';
+
+export const CardQuiz = () => {
+
+    return(
+        <div className="card-quiz">
+            <h2 className="title-quiz">kuala lumpur is the capital of</h2>
+
+            <ButtonsAnswers />
+        </div>
+    )
+}
